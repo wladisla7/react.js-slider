19bfa58530eb414a8a93818615f9e79fa0100671
f0rest <wladislawfedoseev@gmail.com>
2021-10-18 13:31:18 +0300

    addChangeWithPicture

--- src/components/slider/index.jsx
+++ src/components/slider/index.jsx
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
+import firstimg from '../../assets/images/11.png';
+import secondimg from '../../assets/images/12.png';
+import thirdimg from '../../assets/images/13.png';
+import fourimg from '../../assets/images/14.png';
+import fiveimg from '../../assets/images/15.png';
+
 
 const arrayImages = [
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-    "https://cdn.pixabay.com/photo/2021/08/25/20/42/field-6574455__480.jpg",
-    "https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270",
+    firstimg,
+    secondimg,
+    thirdimg,
+    fourimg,
+    fiveimg,
 ];
 
 export default function Slider() {
@@ -35,6 +43,9 @@ export default function Slider() {
     }, [currentSlide]);
     return (
         <div>
+            <div className="options">
+                
+            </div>
             <div className="display">
                 <button onClick={prevSlide}>Prev</button>
                 <img src={arrayImages[currentSlide]} alt="" />
